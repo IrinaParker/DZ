62fa6229214c652d9817f64f29eb58f0dfcfe8b5
Irina Ruchkina <irinary@mail.ru>
2023-02-15 23:56:30 +0300

    зад19

--- задача15/Program.cs
+++ задача15/Program.cs
@@ -5,10 +5,10 @@ Console.WriteLine("Ведите цифру дня недели от 1 до 7: ")
 int number = int.Parse(Console.ReadLine()!);
 
 if (number == 6 || number == 7)
-{
+    {
     Сonsole.WriteLine("Да");
-}
+    }
 else
-{
+    {
     Сonsole.WriteLine("НЕТ");
-}
+    }
---
+++ задача19/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. 14212 -> нет 12821 -> да
+
+Console.WriteLine("Введите пятизначное число        ");
+int number = int.Parse(Console.ReadLine()!);
+int number1 = 0;
+int number2 = 0;
+int number3 = 0;
+int number4 = 0;
+
+number1 = number / 10000;
+number2 = number % 10;
+number3 = number / 1000 % 10;
+number4 = number % 100 / 10;
+
+if (number1 == number2 & number3 = number4) 
+    {
+        Console.Write("Полиндром");
+    }
+else
+    {
+        Console.Write("НЕ полиндром");
+    }
+
+
+
+
